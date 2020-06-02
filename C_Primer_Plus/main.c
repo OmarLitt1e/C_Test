3ecaa479af188593d7f7a5f4f0100332b8248ba9
OmarLitt1e <zhjomar@163.com>
2020-06-02 11:38:36 +0800

    1. section 4 code

--- C_Primer_Plus/main.c
+++ C_Primer_Plus/main.c
@@ -1,115 +1,129 @@
-///////////3.11-8//////////////
+///////////////4.8-8/////////////////
 #include <stdio.h>
+#include <float.h>
 
 int main(void)
 {
+    float mile;
+    float gallon;
 
-    float Cup;
+    printf("Enter mile and gas gallon\n");
+    scanf("%f%f",&mile,&gallon);
+    printf("mile per gallon is %.1f\n",mile/gallon);
+    printf("kilometer per litre is %.1f\n",(mile*1.609)/(gallon*3.785));
 
-    printf("Enter the cup:  ");
-    scanf("%f\n",&Cup);
-    printf("The pint number of cup is %f\n",Cup*2.0);
-    printf("The Ounce number of cup is %f\n",Cup/8.0);
-    printf("The SoupLadle number of cup is %f\n",(Cup/8.0)*2.0);
-    printf("The Teaspoon number of cup is %f\n",(Cup/8.0)*2.0/3.0);
     return 0;
 }
 
-/////////////3.11-7//////////////
+/////////////////4.8-7/////////////////
+//#include <stdio.h>
+//#include <float.h>
+//
+//int main(void)
+//{
+//    double a=1.0/3.0;
+//    float b=1.0/3.0;
+//
+//    printf("%.6f\t%.6f\n",a,b);
+//    printf("%.12f\t%.12f\n",a,b);
+//    printf("%.16f\t%.16f\n",a,b);
+//    printf("FLT_DIG is %d\nDBL_DIG is %d\n",FLT_DIG,DBL_DIG);
+//    return 0;
+//}
+
+/////////////////4.8-6/////////////////
+//#include <stdio.h>
+//#include <string.h>
+//int main(void)
+//{
+//    float rate;
+//    float size;
+//    char FirstName[10];
+//    char LastName[10];
+//    printf("Enter your first name\n");
+//    scanf("%s",FirstName);
+//    printf("Enter your last name\n");
+//    scanf("%s",LastName);
+//    printf("%s %s\n",LastName,FirstName);
+//    printf("%d\t%d\n",(unsigned char)strlen(LastName),(unsigned char)strlen(FirstName));
+//
+//    printf("%-s\t%-s\n",LastName,FirstName);
+//    printf("%-d\t%-d\n",(unsigned char)strlen(LastName),(unsigned char)strlen(FirstName));
+//
+//    return 0;
+//}
+
+/////////////////4.8-5/////////////////
 //#include <stdio.h>
 //
 //int main(void)
 //{
+//    float rate;
+//    float size;
 //
-//    float CMPerInch=2.54;
-////    unsigned char Stature_Inch;
-////    printf("Enter your stature in inch:  ");
-////    scanf("%u\n",&Stature_Inch);
-//    float Stature_Inch;
-//    printf("Enter your stature in inch:  ");
-//    scanf("%f\n",&Stature_Inch);
-//    printf("Your stature in CM is %f\n",Stature_Inch*CMPerInch);
+//    printf("Enter rate and size\n");
+//    scanf("%f%f",&rate,&size);
+//    printf("At %.2f megabits per second, a file of %.2f megabytes"
+//    " downloads in %.2f seconds\n",
+//    rate,size,size*8.0/rate);
+//
 //    return 0;
 //}
 
-/////////////3.11-6//////////////
+/////////////////4.8-4/////////////////
 //#include <stdio.h>
 //
 //int main(void)
 //{
+//    float stature;
+//    char name[20];
+//    printf("Enter your stature and name\n");
+//    scanf("%f%s",&stature,name);
+//    printf("%s, you are %.3f feet tall\n",name,stature);
 //
-//    float H2OMole_PerQuart=950.0/(3.0e-23);
-//    unsigned char Quart;
-//    printf("Enter the number of H2O quart:  ");
-//    scanf("%u\n",&Quart);
-//    printf("The number of H2O molecules correspond to H2O quart is %f\n",H2OMole_PerQuart*Quart);
 //    return 0;
 //}
 
-/////////////3.11-5//////////////
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//
-//    float SecOfYear=3.156e7;
-//    float age;
-//    printf("Enter your age:  ");
-//    scanf("%f\n",&age);
-//    printf("The Seconds correspond to your age is %f\n",age*SecOfYear);
-//    return 0;
-//}
-
-/////////////3.11-4//////////////
+/////////////////4.8-3/////////////////
 //#include <stdio.h>
 //
 //int main(void)
 //{
 //    float a;
-//    printf("Enter a floating-point value:  ");
-//    scanf("%f\n",&a);
-//    printf("fixed-point notation:  %f\n",a);
-//    printf("exponential notation:  %e\n",a);
-//    //printf("p notation:  %a\n",a);
-//    return 0;
-//}
-
-/////////////3.11-3//////////////
-//#include <stdio.h>
 //
-//int main(void)
-//{
-//    char cBell=0x07;
-//    printf("%c",cBell);
-//    printf("Startled by the sudden sound, Sally shouted,\n");
-//    printf("\"By the Great Pumpkin, what was that!\"\n");
-//    return 0;
-//}
-/////////////3.11-2//////////////
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//    char ch;
-//    printf("Enter a decimal value of ASCII\n");
-//    scanf("%d\n",&ch);
-//    printf("The input ASCII is %c\n",ch);
+//    scanf("%f",&a);
+//    printf("%e\n",a);
+//    printf("%+f\n",a);
 //
 //    return 0;
 //}
 
-/////////////3.11-1//////////////
+/////////////////4.8-2/////////////////
 //#include <stdio.h>
 //
 //int main(void)
 //{
-//    float a;
-//    double b;
-////    unsigned char b;
-////    b=sizeof(float);
-//    a=2.000003;
-//    b=22222.000003;
-//    printf("%f\t%f\n",a,b);
+//    char Name[20];
 //
+//    printf("Enter your name\n");
+//    scanf("%s",Name);
+//    printf("\"%20s\"\n",Name);
+//    printf("\"%15s\"\n",Name);
+//    //printf("\"%s%s\"\n",FirstName,LastName);
+//
+//
+//    return 0;
+//}
+
+/////////////////4.8-1/////////////////
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    char FirstName[20];
+//    char LastName[20];
+//    printf("Enter your first name and last name\n");
+//    scanf("%s%s",FirstName,LastName);
+//    printf("Your name is \"%s,%s\"\n",FirstName,LastName);
 //    return 0;
 //}
