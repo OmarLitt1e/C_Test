7c67af0e3217f05e0e9c7bbaaa3c6b9633c7fbde
OmarLitt1e <zhjomar@163.com>
2020-06-12 16:46:36 +0800

    1. add section 8 code

--- C_Primer_Plus/main.c
+++ C_Primer_Plus/main.c
@@ -1,160 +1,64 @@
-/////////////7.12-11////////////
+
+
+/////////////8.11-8////////////
 #include <stdio.h>
 #include <float.h>
-#define ArtichPrice 2.05
-#define BeetPrice   1.15
-#define CarotaPrice 1.09
-#define Ship5P      6.5
-#define Ship5to20P  14
-#define Ship20P     0.5
 
 int main(void)
 {
-    unsigned char Choice;
-    float a,b,c,ArtichWgt,BeetWgt,CarotaWgt,GrossWgt;
-    float VegeCost,Discount,ShipCost;
-    ArtichWgt=BeetWgt=CarotaWgt=Discount=ShipCost=0;
-    while((Choice=getchar())!='q')
+    char ch;
+    float a,b,c;
+    printf("Enter the calculation category:\n"
+           "1) addition         2) subtraction\n"
+           "3) multiplication   4) division\n"
+           "q) quit\n");
+    while((ch=getchar())!=EOF)
     {
-        if(Choice=='\n')
+        if(ch=='\n')
             continue;
-        switch(Choice)
+        if(ch=='1' || ch=='2' || ch=='3' || ch=='4')
         {
-        case 'a':
-            printf("Enter the weight you want for Artichoke:\n");
-            scanf("%f",&a);
-            ArtichWgt+=a;
-            break;
-        case 'b':
-            printf("Enter the weight you want for Beet:\n");
-            scanf("%f",&b);
-            BeetWgt+=b;
-            break;
-        case 'c':
-            printf("Enter the weight you want for Carota:\n");
-            scanf("%f",&c);
-            CarotaWgt+=c;
-            break;
-        default:
-            ;
-        }
+            printf("Enter 2 num for calculation, 'q' for quit\n");
+            while(2==scanf("%f%f",&a,&b))
+            {
+                switch(ch)
+                {
+                    case '1':
+                        c=a+b;
+                        break;
+                    case '2':
+                        c=a-b;
+                        break;
+                    case '3':
+                        c=a*b;
+                        break;
+                    case '4':
+                        while(b<=FLT_EPSILON && b>=-FLT_EPSILON)
+                        {
+                            printf("Divisor can not be 0!\n"
+                                   "Enter another divisor"
+                                   " or input q to quit this calculation\n");
+                            if(1==scanf("%f",&b))
+                                ;
+                            else
+                                printf("Wrong format, enter again!\n");
 
+                        }
+                        c=a/b;
+                        break;
+                    default:
+                        ;
+                }
+            }
+            printf("%f\n",c);
+        }
     }
-    if((VegeCost=ArtichWgt*ArtichPrice+BeetWgt*BeetPrice+CarotaWgt*CarotaPrice)>100.0)
-        Discount=VegeCost*0.05;
-    if((GrossWgt=ArtichWgt+BeetWgt+CarotaWgt)<=5.0)
-        ShipCost=Ship5P;
-    else if(GrossWgt>5.0 && GrossWgt<=20)
-        ShipCost=Ship5to20P;
-    else
-        ShipCost=Ship5to20P+(GrossWgt-20.0)*0.5;
-    printf("Here's your shopping list:\n");
-    printf("Vegetable\tWeight\tCost\n");
-    printf("Artichoke:\t%.2f\t%.3f\n",ArtichWgt,ArtichPrice*ArtichWgt);
-    printf("Beet:     \t%.2f\t%.3f\n",BeetWgt,BeetPrice*BeetWgt);
-    printf("Carota:   \t%.2f\t%.3f\n",CarotaWgt,CarotaPrice*CarotaWgt);
-    printf("Vegetable cost: %.3f\n",VegeCost);
-    printf("Discount: %.3f\n",Discount);
-    printf("Shipment cost: %.3f\n",ShipCost);
-    printf("Gross cost: %.3f\n",VegeCost-Discount+ShipCost);
+
     return 0;
 }
 
-///////////////7.12-10////////////
-//#include <stdio.h>
-//#include <float.h>
-//#define BaseForSingle 17850.0
-//#define BaseForHsholder 23900.0
-//#define BaseForMarried 29750.0
-//#define BaseForDivorced 14875.0
-//
-//int main(void)
-//{
-//    unsigned char Status;
-//    float Salary;
-//
-//    printf("Enter your status and you salary:\n"
-//           "A single    B householder\n"
-//           "C married   D divorced\n");
-//   while(2==scanf("%c%f",&Status,&Salary))
-//   {
-//       switch(Status)
-//       {
-//            case 'A':
-//               if(Salary-BaseForSingle<FLT_EPSILON)
-//                    printf("Your taxes is %.2f\n",Salary*0.15);
-//               else
-//                    printf("Your taxes is %.2f\n",0.15*BaseForSingle+(Salary-BaseForSingle)*0.28);
-//               break;
-//            case 'B':
-//               if(Salary-BaseForHsholder<FLT_EPSILON)
-//                    printf("Your taxes is %.2f\n",Salary*0.15);
-//               else
-//                    printf("Your taxes is %.2f\n",0.15*BaseForHsholder+(Salary-BaseForHsholder)*0.28);
-//               break;
-//            case 'C':
-//               if(Salary-BaseForMarried<FLT_EPSILON)
-//                    printf("Your taxes is %.2f\n",Salary*0.15);
-//               else
-//                    printf("Your taxes is %.2f\n",0.15*BaseForMarried+(Salary-BaseForMarried)*0.28);
-//               break;
-//            case 'D':
-//               if(Salary-BaseForDivorced<FLT_EPSILON)
-//                    printf("Your taxes is %.2f\n",Salary*0.15);
-//               else
-//                    printf("Your taxes is %.2f\n",0.15*BaseForDivorced+(Salary-BaseForDivorced)*0.28);
-//               break;
-//            default:
-//                ;
-//
-//       }
-//   }
-//    return 0;
-//}
 
-///////////////7.12-9////////////
-//#include <stdio.h>
-//
-//
-//
-//int main(void)
-//{
-//    int PosiInteger;
-//    unsigned int i,j;
-//    unsigned char PrimeFlag=0;
-//
-//    printf("Enter a positive integer:\n");
-//    while(1==scanf("%d",&PosiInteger)  && PosiInteger>0)
-//    {
-//        if(PosiInteger==1 || PosiInteger==2 || PosiInteger==3 )
-//            printf("%d\n",PosiInteger);
-//        else
-//        {
-//            for(i=4;i<=PosiInteger;i++)
-//            {
-//                for(j=2;j<i;j++)
-//                {
-//                    if(i%j==0)
-//                    {
-//                        PrimeFlag=0;
-////                        printf("It's not prime number, enter anothers\n");
-//                        break;
-//                    }
-//                    else
-//                    {
-//                        PrimeFlag=1;
-//                    }
-//                }
-//                if(1==PrimeFlag)
-//                    printf("%d  ",i);
-//            }
-//        }
-//    }
-//
-//    return 0;
-//}
-
-///////////////7.12-8////////////
+///////////////8.11-7////////////
 //#include <stdio.h>
 //#define FirstOption 8.75
 //#define SecondOption 9.33
@@ -173,7 +77,7 @@ int main(void)
 //    printf("Enter the number corresponding to the desired pay rate or action:\n");
 //    printf("1)$8.75/hr\t\t2)$9.33/hr\n"
 //           "3)$10.00/hr\t\t4)$11.20/hr\n"
-//           "5)quit\n");
+//           "q)quit\n");
 //    printf("******************************\n");
 //    while((Option=getchar())!='#')
 //    {
@@ -215,7 +119,7 @@ int main(void)
 //                   "Your net income is %.2f\n"
 //                   ,GrossSalary,Taxes,GrossSalary-Taxes);
 //        }
-//        else if(Option=='5')
+//        else if(Option=='q')
 //        {
 //            printf("quit\n");
 //            break;
@@ -229,183 +133,241 @@ int main(void)
 //}
 
 
-///////////////7.12-7////////////
+///////////////8.11-6////////////
 //#include <stdio.h>
-//#define BaseSalary 1000.0
+//
+//char get_first(void);
+//
 //int main(void)
 //{
-//    float GrossSalary,Taxes;
-//    unsigned char WorkHour=0;
-//    printf("Enter you work hours:\n");
-//    scanf("%u",&WorkHour);
-//    if(40>=WorkHour)
-//        GrossSalary=BaseSalary*WorkHour;
-//    else
-//        GrossSalary=BaseSalary*40.0+BaseSalary*1.5*(WorkHour-40);
-//    if(GrossSalary<=300.0)
-//        Taxes=0.15*GrossSalary;
-//    else if(GrossSalary>300 && GrossSalary<=450)
-//        Taxes=0.15*300+(GrossSalary-300)*0.2;
-//    else if(GrossSalary>450)
-//        Taxes=0.15*300+0.2*150+(GrossSalary-450)*0.25;
 //
-//    printf("Your gross salary is %.2f\n"
-//           "Your Taxes is %.2f\n"
-//           "Your net income is %.2f\n"
-//           ,GrossSalary,Taxes,GrossSalary-Taxes);
+//    putchar(get_first());
 //
 //    return 0;
 //}
+//char get_first(void)
+//{
+//    int ch;
+//    while ((ch=getchar()) == ' ' || ch =='\n')
+//        continue;
+//    return ch;
+//}
 
-///////////////7.12-6////////////
+///////////////8.11-5////////////
 //#include <stdio.h>
+//
 //int main(void)
 //{
+//    int guess = 50;
 //    char ch;
-//    char i;
-//    unsigned char record;
-//    unsigned char eiNum=0;
-//    while((ch=getchar())!='#')
+//    printf("Pick an integer from 1 to 100. I will try to guess ");
+//    printf("it.\nRespond with a y if my guess is right, with");
+//    printf("\na b if it is bigger,with a s if it is smallar .\n");
+//    printf("Uh...is your number %d?\n", guess);
+//    while ((ch=getchar()) != 'y')
 //    {
-//        printf("%c",ch);
-//        if('e'==ch)
-//            record=1;
-//        if(1==record)
+//        if(ch=='\n')
+//            continue;
+//        if(ch=='b')
 //        {
-//            if('i'==ch)
-//                eiNum++;
+//            printf("Is it %d?\n",guess/=2);
 //        }
+//        else if(ch=='s')
+//            printf("Is it %d?\n",guess=(100+guess)/2);
+//        else
+//            printf("Enter the right answer! y b or s\n");
 //    }
-//    printf("\nei occur %u times\n",eiNum);
+//
 //    return 0;
 //}
 
-///////////////7.12-5////////////
+///////////////8.11-4////////////
 //#include <stdio.h>
+//#include <ctype.h>
+//
 //int main(void)
 //{
 //    char ch;
-//    char i;
-//    while((ch=getchar())!='#')
+//    unsigned char InWord,Counter;
+//    InWord=Counter=0;
+//    while((ch=getchar())!=EOF)
 //    {
-//        switch (ch)
+//        if(ch=='\n')
+//            continue;
+//
+//        if(ch==' ' && InWord==1)
 //        {
-//            case ',':
-//                ch='!';
-//                i++;
-//                printf("%c",ch);
+//            printf("%u\n",Counter);
+//            Counter=0;
+//        }
+//        (ch!=' ') ? (InWord=1) : (InWord=0);
+//        if(InWord==1)
+//            Counter++;
+//
+//    }
+//
+//    return 0;
+//}
+
+///////////////8.11-3////////////
+//#include <stdio.h>
+//#include <ctype.h>
+//
+//int main(void)
+//{
+//    char ch;
+//    unsigned char Counter_Upper,Counter_Lower;
+//    Counter_Upper=Counter_Lower=0;
+//    while((ch=getchar())!=EOF)
+//    {
+//        if(ch<'A' || (ch>'Z' && ch<'a') || ch>'z')
+//            continue;
+//        if(isupper(ch))
+//            Counter_Upper++;
+//        else if(islower(ch))
+//            Counter_Lower++;
+//
+//    }
+//    printf("Num of Upper is %u\n"
+//           "Num of Lower is %u\n",
+//           Counter_Upper,Counter_Upper);
+//
+//
+//
+//    return 0;
+//}
+
+///////////////8.11-2////////////
+//#include <stdio.h>
+//
+//
+//int main(void)
+//{
+//    char ch;
+//    unsigned char i=0;
+//    while((ch=getchar())!=EOF)
+//    {
+//        if(i++==10)
+//            putchar('\n');
+//        switch(ch)
+//        {
+//            case ' ':
+//                printf("Space %d ",ch);
 //                break;
-//            case '!':
-//                i++;
-//                printf("%c",ch);
-//                printf("%c",ch);
+//            case '\n':
+//                putchar('\\');
+//                printf("n %d ",ch);
+//                break;
+//            case '\t':
+//                putchar('\\');
+//                printf("t %d ",ch);
 //                break;
 //            default:
-//                printf("%c",ch);
+//                printf("%c %d ",ch,ch);
 //        }
 //    }
-//    printf("\n");
-//    printf("Taken place happened %d times\n",i);
 //    return 0;
 //}
 
-///////////////7.12-4////////////
+///////////////FuXiTi-3 && 8.11-1////////////
 //#include <stdio.h>
+//
+//
 //int main(void)
 //{
 //    char ch;
-//    char i;
-//    while((ch=getchar())!='#')
+//    unsigned char Counter=0;
+//    while((ch=getchar())!=EOF)
 //    {
-//        if(','==ch)
-//        {
-//            ch='!';
-//            i++;
-//            printf("%c",ch);
-//        }
-//        else if('!'==ch)
-//        {
-//            i++;
-//            printf("%c",ch);
-//            printf("%c",ch);
-//        }
-//        else
-//        {
-//            printf("%c",ch);d
-//        }
+//        if(ch!=' ' && ch!='\n')
+//            Counter++;
 //    }
-//    printf("\n");
-//    printf("Taken place happened %d times\n",i);
+//    printf("There are %u characters in file essay\n",Counter);
 //    return 0;
 //}
 
-///////////////7.12-3////////////
-//#include <stdio.h>
-//int main(void)
-//{
-//    char ch;
-//    char OddNum,EvenNum;
-//    int OddSum,EvenSum;
-//    OddSum=0;
-//    EvenSum=0;
-//    while((ch=getchar())!='0')
-//    {
-//        if(ch=='\n')
-//            continue;
-//        if(ch%2==0)
-//        {
-//            EvenNum++;
-//            EvenSum+=ch;
-//        }
-//        else
-//        {
-//            OddNum++;
-//            OddSum+=ch;
-//        }
-//    }
-//    printf("Num of Even is %d,average num of Even is %.2f\n"
-//           "Num of Odd is %d,average num of Odd is %.2f\n",
-//           EvenNum,((double)EvenSum/(double)EvenNum)-48.0,OddNum,((double)OddSum/(double)OddNum)-48.0);
-//    return 0;
-//}
 
-///////////////7.12-2////////////
+///////////////8.8////////////
 //#include <stdio.h>
+//
+//char get_choice(void);
+//char get_first(void);
+//int get_int(void);
+//void count(void);
+//
 //int main(void)
 //{
-//    char ch;
-//    char chCounter=0;
-//    while((ch=getchar())!='#')
+//    int choice;
+//    while ((choice = get_choice()) != 'q')
 //    {
-//        if(ch=='\n')
-//            continue;
-//        if(0==chCounter++ % 8)
-//            printf("\n");
-//        printf("%c%d ",ch,ch);
+//        switch (choice)
+//        {
+//            case 'a': printf("Buy low, sell high.\n");
+//            break;
+//            case 'b': putchar('\a'); /* ANSI */
+//            break;
+//            case 'c': count();
+//            break;
+//            default: printf("Program error!\n");
+//            break;
+//        }
 //    }
+//    printf("Bye.\n");
 //
 //    return 0;
 //}
-
-///////////////7.12-1////////////
-//#include <stdio.h>
-//int main(void)
-//{
-//    char ch;
-//    int SpaceNum=0;
-//    int LFNum=0;
-//    int OtherNum=0;
-//    while((ch=getchar())!='#')
-//    {
-//        if(ch=='\n')
-//            LFNum++;
-//        else if(ch==' ')
-//            SpaceNum++;
-//        else
-//            OtherNum++;
-//    }
-//    printf("SpaceNum is %d\nLFNum is %d\nOtherNum is %d\n",SpaceNum,LFNum,OtherNum);
 //
-//    return 0;
+//void count(void)
+//{
+//    int n, i;
+//    printf("Count how far? Enter an integer:\n");
+//    n = get_int();
+//    for (i = 1; i <= n; i++)
+//        printf("%d\n", i);
+//    while (getchar() != '\n')
+//        continue;
 //}
+//
+//char get_choice(void)
+//{
+//    int ch;
+//    printf("Enter the letter of your choice:\n");
+//    printf("a. advice b. bell\n");
+//    printf("c. count q. quit\n");
+//    ch = get_first();
+//    while ((ch < 'a' || ch > 'c') && ch != 'q')
+//    {
+//        printf("Please respond with a, b, c, or q.\n");
+//        ch = get_first();
+//    }
+//    return ch;
+//}
+//
+//char get_first(void)
+//{
+//    int ch;
+//    while ((ch=getchar()) == '\n')
+//        ;
+////    ch = getchar();
+//    while (getchar() != '\n')
+//        continue;
+//    return ch;
+//}
+//int get_int(void)
+//{
+//    int input;
+//    char ch;
+//    while (scanf("%d", &input) != 1)
+//    {
+//        while ((ch = getchar()) != '\n')
+//            putchar(ch); // ¥¶¿Ì¥ÌŒÛ ‰≥ˆ
+//        printf(" is not an integer.\nPlease enter an ");
+//        printf("integer value, such as 25, -178, or 3: ");
+//    }
+//    return input;
+//}
+//
+
+
+
 
